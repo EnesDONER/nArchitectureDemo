b8ba93a970310f3ed8b85da14c419491ff651797
Enes Doner <businessenesdoner@gmail.com>
2024-06-07 17:22:24 +0300

    Add project files.

---
+++ src/corePackages/Core.Packages/Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,22 @@
+﻿namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+
+    public DateTime? DeletededDate { get; set; }
+
+    public Entity()
+    {
+        Id = default;
+    }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+}
