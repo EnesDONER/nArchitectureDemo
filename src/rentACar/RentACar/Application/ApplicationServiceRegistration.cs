ddaf0319dbba7007d9116028dd77b520e0ed5163
Enes Doner <businessenesdoner@gmail.com>
2024-06-09 15:32:22 +0300

    Create business rule

--- src/rentACar/RentACar/Application/ApplicationServiceRegistration.cs
+++ src/rentACar/RentACar/Application/ApplicationServiceRegistration.cs
@@ -1,11 +1,6 @@
-﻿using Application.Features.Brands.Profiles;
+﻿using Core.CrossCuttingConcerns.Rules;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application
 {
@@ -14,12 +9,29 @@ namespace Application
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinesRules));
 
             services.AddMediatR(configuration =>
             {
                 configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             });
 
+            return services;
+        }
+
+        public static IServiceCollection AddSubClassesOfType(
+           this IServiceCollection services,
+           Assembly assembly,
+           Type type,
+           Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null)
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+                if (addWithLifeCycle == null)
+                    services.AddScoped(item);
+
+                else
+                    addWithLifeCycle(services, type);
             return services;
         }
     }
